54bf569c829a193dd58c9892972dcbb06a27677e
Nick Capps <nickcapps10@gmail.com>
2020-09-23 04:37:14 +0000

    Add Ch06, database manager

--- ch06/database.py
+++ ch06/database.py
@@ -3,71 +3,61 @@ import sqlite3
 
 class DatabaseManager:
     def __init__(self, database_filename):
-        self.connection = sqlite3.connect(database_filename)  # <1>
+        self.connection = sqlite3.connect(database_filename)
 
     def __del__(self):
-        self.connection.close()  # <2>
+        self.connection.close()
 
-    def _execute(self, statement, values=None):  # <1>
+    def _execute(self, statement, values=None):
         with self.connection:
             cursor = self.connection.cursor()
-            cursor.execute(statement, values or [])  # <2>
+            cursor.execute(statement, values or [])
             return cursor
 
     def create_table(self, table_name, columns):
-        columns_with_types = [  # <1>
-            f'{column_name} {data_type}'
-            for column_name, data_type in columns.items()
-        ]
-        self._execute(  # <2>
+        columns_with_types = [f'{column_name}' f'{data_type}'
+                              for column_name, data_type in columns.items()]
+        self._execute(
             f'''
             CREATE TABLE IF NOT EXISTS {table_name}
-            ({', '.join(columns_with_types)});
+            ({", ".join(columns_with_types)});'
             '''
         )
 
-    def drop_table(self, table_name):
-        self._execute(f'DROP TABLE {table_name};')
-
     def add(self, table_name, data):
         placeholders = ', '.join('?' * len(data))
-        column_names = ', '.join(data.keys())  # <1>
-        column_values = tuple(data.values())  # <2>
-
+        column_names = ', '.join(data.keys())
+        column_values = tuple(data.values())
         self._execute(
             f'''
             INSERT INTO {table_name}
             ({column_names})
             VALUES ({placeholders});
             ''',
-            column_values,  # <3>
+            column_values
         )
 
-    def delete(self, table_name, criteria):  # <1>
-        placeholders = [f'{column} = ?' for column in criteria.keys()]
+    def delete(self, table_name, criteria):
+        placeholders = [f'{column_name} = ?'
+                        for column_name in criteria.keys()]
         delete_criteria = ' AND '.join(placeholders)
         self._execute(
             f'''
-            DELETE FROM {table_name}
+            DELETE {table_name}
             WHERE {delete_criteria};
             ''',
-            tuple(criteria.values()),  # <2>
+            tuple(criteria.values())
         )
 
     def select(self, table_name, criteria=None, order_by=None):
-        criteria = criteria or {}  # <1>
-
-        query = f'SELECT * FROM {table_name}'
-
-        if criteria:  # <2>
-            placeholders = [f'{column} = ?' for column in criteria.keys()]
-            select_criteria = ' AND '.join(placeholders)
-            query += f' WHERE {select_criteria}'
-
-        if order_by:  # <3>
-            query += f' ORDER BY {order_by}'
-
-        return self._execute(  # <4>
-            query,
-            tuple(criteria.values()),
+        placeholders = [f'{column_name} = ?'
+                        for column_name in criteria.keys()]
+        select_criteria = ' AND '.join(placeholders)
+        return self._execute(
+            f'''
+            SELECT * FROM {table_name}
+            {"WHERE " + select_criteria if criteria else ""}
+            {"ORDER BY " + order_by if order_by else ""};
+            ''',
+            tuple(criteria.values())
         )
